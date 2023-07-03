45577f9fd10b350531adcb7607b2bed38ecbb9f9
Furkan Tekin <tekinfurkan@outlook.com>
2023-07-03 16:47:46 +0300

    Code simplified

--- TicTacToe/ViewController.swift
+++ TicTacToe/ViewController.swift
@@ -15,22 +15,13 @@ class ViewController: UIViewController
 	}
 	
 	@IBOutlet weak var turnLabel: UILabel!
-	@IBOutlet weak var a1: UIButton!
-	@IBOutlet weak var a2: UIButton!
-	@IBOutlet weak var a3: UIButton!
-	@IBOutlet weak var b1: UIButton!
-	@IBOutlet weak var b2: UIButton!
-	@IBOutlet weak var b3: UIButton!
-	@IBOutlet weak var c1: UIButton!
-	@IBOutlet weak var c2: UIButton!
-	@IBOutlet weak var c3: UIButton!
-	
+
+    @IBOutlet var Cells : [UIButton]!
+    
 	var firstTurn = Turn.Cross
 	var currentTurn = Turn.Cross
-	
 	var NOUGHT = "O"
 	var CROSS = "X"
-	var board = [UIButton]()
 	
 	var noughtsScore = 0
 	var crossesScore = 0
@@ -38,21 +29,8 @@ class ViewController: UIViewController
 	override func viewDidLoad()
 	{
 		super.viewDidLoad()
-		initBoard()
 	}
 	
-	func initBoard()
-	{
-		board.append(a1)
-		board.append(a2)
-		board.append(a3)
-		board.append(b1)
-		board.append(b2)
-		board.append(b3)
-		board.append(c1)
-		board.append(c2)
-		board.append(c3)
-	}
 
 	@IBAction func boardTapAction(_ sender: UIButton)
 	{
@@ -76,49 +54,48 @@ class ViewController: UIViewController
 		}
 	}
 	
-	func checkForVictory(_ s :String) -> Bool
-	{
-		// Horizontal Victory
-		if thisSymbol(a1, s) && thisSymbol(a2, s) && thisSymbol(a3, s)
-		{
-			return true
-		}
-		if thisSymbol(b1, s) && thisSymbol(b2, s) && thisSymbol(b3, s)
-		{
-			return true
-		}
-		if thisSymbol(c1, s) && thisSymbol(c2, s) && thisSymbol(c3, s)
-		{
-			return true
-		}
-		
-		// Vertical Victory
-		if thisSymbol(a1, s) && thisSymbol(b1, s) && thisSymbol(c1, s)
-		{
-			return true
-		}
-		if thisSymbol(a2, s) && thisSymbol(b2, s) && thisSymbol(c2, s)
-		{
-			return true
-		}
-		if thisSymbol(a3, s) && thisSymbol(b3, s) && thisSymbol(c3, s)
-		{
-			return true
-		}
-		
-		// Diagonal Victory
-		if thisSymbol(a1, s) && thisSymbol(b2, s) && thisSymbol(c3, s)
-		{
-			return true
-		}
-		if thisSymbol(a3, s) && thisSymbol(b2, s) && thisSymbol(c1, s)
-		{
-			return true
-		}
-		
-		return false
-	}
-	
+    func checkForVictory(_ s :String) -> Bool
+        {
+            // Horizontal Victory
+            if thisSymbol(Cells[0], s) && thisSymbol(Cells[1], s) && thisSymbol(Cells[2], s)
+            {
+                return true
+            }
+            if thisSymbol(Cells[3], s) && thisSymbol(Cells[4], s) && thisSymbol(Cells[5], s)
+            {
+                return true
+            }
+            if thisSymbol(Cells[6], s) && thisSymbol(Cells[7], s) && thisSymbol(Cells[8], s)
+            {
+                return true
+            }
+            
+            // Vertical Victory
+            if thisSymbol(Cells[0], s) && thisSymbol(Cells[3], s) && thisSymbol(Cells[6], s)
+            {
+                return true
+            }
+            if thisSymbol(Cells[1], s) && thisSymbol(Cells[4], s) && thisSymbol(Cells[7], s)
+            {
+                return true
+            }
+            if thisSymbol(Cells[2], s) && thisSymbol(Cells[5], s) && thisSymbol(Cells[8], s)
+            {
+                return true
+            }
+            
+            // Diagonal Victory
+            if thisSymbol(Cells[0], s) && thisSymbol(Cells[4], s) && thisSymbol(Cells[8], s)
+            {
+                return true
+            }
+            if thisSymbol(Cells[2], s) && thisSymbol(Cells[4], s) && thisSymbol(Cells[6], s)
+            {
+                return true
+            }
+            
+            return false
+        }
 	func thisSymbol(_ button: UIButton, _ symbol: String) -> Bool
 	{
 		return button.title(for: .normal) == symbol
@@ -136,10 +113,10 @@ class ViewController: UIViewController
 	
 	func resetBoard()
 	{
-		for button in board
+		for i in Cells
 		{
-			button.setTitle(nil, for: .normal)
-			button.isEnabled = true
+			i.setTitle(nil, for: .normal)
+			i.isEnabled = true
 		}
 		if firstTurn == Turn.Nought
 		{
@@ -156,9 +133,9 @@ class ViewController: UIViewController
 	
 	func fullBoard() -> Bool
 	{
-		for button in board
+		for j in Cells
 		{
-			if button.title(for: .normal) == nil
+			if j.title(for: .normal) == nil
 			{
 				return false
 			}
